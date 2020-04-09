70494fd7a57335ba0a34d719117937a58ae0a5f0
louiscklaw <louiscklaw@gmail.com>
2020-04-09 15:14:58 +0800

    ci: update and test,

--- travis-broadcast/broadcast_test.sh
+++ travis-broadcast/broadcast_test.sh
@@ -2,12 +2,10 @@
 
 set -ex
 
-cd travis-broadcast
-  apt update
-  apt install -y python3 python3-pip python3-dev python3-wheel python3-setuptools
-  pip3 install --user pipenv
+apt update
+apt install -y python3 python3-pip python3-dev python3-wheel python3-setuptools
+pip3 install --user pipenv
 
-  pipenv sync
-  pipenv run python3 travis-broadcast.py \#travis-build-result "testing post to travis-build-result"
-  pipenv run python3 travis-broadcast.py need_help "testing post to travis-need-help"
-cd -
+pipenv sync
+pipenv run python3 travis-broadcast.py \#travis-build-result "testing post to travis-build-result"
+pipenv run python3 travis-broadcast.py need_help "testing post to travis-need-help"
