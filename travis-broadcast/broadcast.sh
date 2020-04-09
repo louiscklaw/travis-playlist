6a4e3cd8a8d236ae345deb3576b17c7fe391e536
louiscklaw <louiscklaw@gmail.com>
2020-04-09 14:45:45 +0800

    ci: update to test broadcast with wget,

--- travis-broadcast/broadcast.sh
+++ travis-broadcast/broadcast.sh
@@ -2,4 +2,18 @@
 
 set -ex
 
-echo "hellobroadcast"
+cd .travis
+  wget https://raw.githubusercontent.com/louiscklaw/travis-playlist/master/travis-broadcast/Pipfile
+  wget https://raw.githubusercontent.com/louiscklaw/travis-playlist/master/travis-broadcast/Pipfile.lock
+  wget https://raw.githubusercontent.com/louiscklaw/travis-playlist/master/travis-broadcast/travis-broadcast.py
+
+  chmod +x travis-broadcast.py
+
+  apt update
+  apt install -y python3 python3-pip python3-dev python3-wheel python3-setuptools
+  pip3 install --user pipenv
+
+  pipenv sync
+  pipenv run python3 travis-broadcast.py \#travis-build-result "testing post to travis-build-result"
+  pipenv run python3 travis-broadcast.py need_help "testing post to travis-need-help"
+cd -
