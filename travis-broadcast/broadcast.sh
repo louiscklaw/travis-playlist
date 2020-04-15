1e9fa7fa37a877bfb1a84c31b78859df584ada0a
louiscklaw <louiscklaw@gmail.com>
2020-04-16 07:01:47 +0800

    ci: init change and test,

--- travis-broadcast/broadcast.sh
+++ travis-broadcast/broadcast.sh
@@ -2,18 +2,15 @@
 
 set -ex
 
-cd .travis
-  wget https://raw.githubusercontent.com/louiscklaw/travis-playlist/master/travis-broadcast/Pipfile
-  wget https://raw.githubusercontent.com/louiscklaw/travis-playlist/master/travis-broadcast/Pipfile.lock
-  wget https://raw.githubusercontent.com/louiscklaw/travis-playlist/master/travis-broadcast/travis-broadcast.py
+wget https://raw.githubusercontent.com/louiscklaw/travis-playlist/master/travis-broadcast/Pipfile
+wget https://raw.githubusercontent.com/louiscklaw/travis-playlist/master/travis-broadcast/Pipfile.lock
+wget https://raw.githubusercontent.com/louiscklaw/travis-playlist/master/travis-broadcast/travis-broadcast.py
 
-  chmod +x travis-broadcast.py
+chmod +x travis-broadcast.py
 
-  sudo apt update
-  sudo apt install -y python3 python3-pip python3-dev python3-wheel python3-setuptools
-  pip3 install --user pipenv
+sudo apt update
+sudo apt install -y python3 python3-pip python3-dev python3-wheel python3-setuptools
+pip3 install --user pipenv
 
-  pipenv sync
-  pipenv run python3 travis-broadcast.py \#travis-build-result "testing post to travis-build-result"
-  pipenv run python3 travis-broadcast.py need_help "testing post to travis-need-help"
-cd -
+pipenv sync
+pipenv run python3 travis-broadcast.py $1 $2
