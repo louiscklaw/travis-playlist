410210976b43d84616c1afc8e27674feb09c01ce
louiscklaw <louiscklaw@gmail.com>
2020-04-16 17:44:51 +0800

    ci: update and test trigger build,

--- Integration_to_other_repo/curl_trigger_portfolio_preact_build.sh
+++ Integration_to_other_repo/curl_trigger_portfolio_preact_build.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 
+# https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&ved=2ahUKEwjjmNvM0OzoAhW0L6YKHSTpA80QFjAAegQIQBAB&url=https%3A%2F%2Fdocs.travis-ci.com%2Fuser%2Ftriggering-builds%2F&usg=AOvVaw3Wws4vYIrJjOWVBGeQ91oa
+
 set -ex
 
 body='{
-"request": {
-"branch":" integration/test-from-travis-playlist"
-}}'
+  "request": {
+    "branch":" integration/test-from-travis-playlist"
+  },
+  "config": {
+    "merge_mode": "deep_merge",
+    "env": {
+      "jobs": [
+        "DRY_RUN=TRUE"
+      ]
+    }
+  }
+}'
 
 curl -s -X POST \
-   -H "Content-Type: application/json" \
-   -H "Accept: application/json" \
-   -H "Travis-API-Version: 3" \
-   -H "Authorization: token ${TRAVIS_AUTH_TOKEN}" \
-   -d "$body" \
-   https://api.travis-ci.com/repo/$1/requests
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token ${TRAVIS_AUTH_TOKEN}" \
+  -d "$body" \
+  https://api.travis-ci.com/repo/$1/requests
