3ac0cc395816748f000eba1f49d7c1fba6ff3f89
louiscklaw <louiscklaw@gmail.com>
2020-04-16 17:56:57 +0800

    ci: fix branch name,

--- Integration_to_other_repo/curl_trigger_portfolio_preact_build.sh
+++ Integration_to_other_repo/curl_trigger_portfolio_preact_build.sh
@@ -6,7 +6,7 @@ set -ex
 
 body='{
   "request": {
-    "branch":" develop",
+    "branch":"develop",
     "message": "trigger from integration test by travis-playlist",
     "config": {
       "merge_mode": "deep_merge",
