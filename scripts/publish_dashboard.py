f294a6130c6263fa761379005449d90946ad7c8c
louiscklaw <louiscklaw@gmail.com>
2020-05-03 23:57:05 +0800

    fix: update and test,

--- scripts/publish_dashboard.py
+++ scripts/publish_dashboard.py
@@ -24,7 +24,7 @@ with lcd('build-dashboard'):
   # directory should exist
   local('ls -l {}'.format(GH_PAGES_DIR))
 
-  local('git checkout --orphan gh-pages')
+  local('git checkout -b --orphan gh-pages')
 
   local('git worktree add {} gh-pages'.format(GH_PAGES_DIR))
 
