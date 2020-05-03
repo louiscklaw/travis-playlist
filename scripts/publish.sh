0b33bc2e9a5f384e08ca742a5eed8651f753cc8b
louiscklaw <louiscklaw@gmail.com>
2020-05-03 14:25:22 +0800

    ci: update publish_dashboard.py,

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,26 +3,38 @@
 set -ex
 
 TMPDIR=$(mktemp -d)
+GH_PAGES_DIR=$TMPDIR/gh-pages
 
 cd build-dashboard
   git submodule update --init --recursive themes/minimal
 
   git worktree prune
 
-  mkdir -p ../../gh-pages
-  git worktree add ../../gh-pages gh-pages
+  mkdir -p $GH_PAGES_DIR
 
-  hugo --minify --enableGitInfo --ignoreCache -d ../../gh-pages
+  # directory should exist
+  ls -l $GH_PAGES_DIR
 
-    cd ../../gh-pages
+  git worktree add $GH_PAGES_DIR gh-pages
+
+  hugo --minify --enableGitInfo --ignoreCache -d $GH_PAGES_DIR
+
+    cd $GH_PAGES_DIR
       git add .
       git commit -m "publish from publish.sh"
       git push
     cd -
 
-#     rm -rf /tmp/gh-pages
 
-  rm -rf ../../gh-pages
+cd ..
+
+git worktree list
+
+git worktree remove $GH_PAGES_DIR
+
+git worktree list
+
+rm -rf $GH_PAGES_DIR
 # rm -rf $TMPDIR
 
 #done
---
+++ scripts/publish_dashboard.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import os,sys
+from pprint import pprint
+
+from fabric.api import local, lcd, run
+
+TMPDIR=local('mktemp -d',capture=True)
+
+GH_PAGES_DIR=os.path.join('/tmp','gh-pages')
+
+with lcd('build-dashboard'):
+
+  local('git submodule update --init --recursive themes/minimal')
+
+  local('git worktree prune')
+
+  local('mkdir -p {}'.format(GH_PAGES_DIR))
+
+  # directory should exist
+  local('ls -l {}'.format(GH_PAGES_DIR))
+
+  local('git worktree add {} gh-pages'.format(GH_PAGES_DIR))
+
+  local('hugo --minify --enableGitInfo --ignoreCache -d {}'.format(GH_PAGES_DIR))
+
+  with lcd(GH_PAGES_DIR):
+    local('git add .')
+    local('git commit -m "publish from publish.sh"')
+    local('git push')
+
+
+  local('git worktree list')
+
+  local('git worktree remove {}'.format(GH_PAGES_DIR))
+
+  local('git worktree list')
+
+  local('rm -rf {}'.format(GH_PAGES_DIR))
+
+#done
