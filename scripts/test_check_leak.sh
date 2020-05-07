d8b12bbae751ae8cab746941468590b205a0fb7a
louiscklaw <louiscklaw@gmail.com>
2020-05-08 00:49:19 +0800

    ci: update python dependcies,

--- scripts/test_check_leak.sh
+++ scripts/test_check_leak.sh
@@ -2,4 +2,9 @@
 
 set -ex
 
+sudo apt update
+sudo apt install -y python3 python3-pip python3-dev python3-wheel python3-setuptools
+pip3 install --user pipenv
+pipenv sync
+
 python3 travis-check-leak/test_pass.py
