accc734b4337bc61b48fbb6d227ca97c6a5571b0
louiscklaw <louiscklaw@gmail.com>
2020-05-08 13:55:51 +0800

    feat: update for test script,

--- scripts/test_check_leak.sh
+++ scripts/test_check_leak.sh
@@ -2,7 +2,18 @@
 
 set -ex
 
-cd travis-check-leak
+mkdir -p tmp
+
+cd tmp
+  # wget pipfile
+  # wget check-leak.py
+  cp ../travis-check-leak/Pipfile .
+  cp ../travis-check-leak/Pipfile.lock .
+  cp ../travis-check-leak/check-leak.py .
+  cp ../travis-check-leak/test_pass.py .
+
+  cp ../travis-check-leak/leak.js .
+
   pipenv sync
 
   pipenv run python3 test_pass.py
