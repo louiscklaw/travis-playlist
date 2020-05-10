2764297809701b523ec629533819aed78200e885
louiscklaw <louiscklaw@gmail.com>
2020-05-10 17:51:50 +0800

    feat: test update to push on master branch only,

--- travis-build-docker-image/docker_push.sh
+++ travis-build-docker-image/docker_push.sh
@@ -2,9 +2,11 @@
 
 set -ex
 
-docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD
-docker push logickee/travis-python-test
-docker push logickee/travis-ubuntu-test
-docker logout
+cd travis-build-docker-image
+  docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD
+  docker push logickee/travis-python-test
+  docker push logickee/travis-ubuntu-test
+  docker logout
+cd ..
 
 # done
