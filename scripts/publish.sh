e281f647cfee5dc66853f09054ee2cfc93126c11
louiscklaw <louiscklaw@gmail.com>
2020-05-29 20:45:03 +0800

    ci: update and test,

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,43 +2,14 @@
 
 set -ex
 
-# design notes:
-# --------------------------------------
-# 2 running condition
-# 1: running locally for development
-# 2: running on travis master branch
-# those environment outside these 2 are not considered
-
-wget https://github.com/gohugoio/hugo/releases/download/v0.69.2/hugo_extended_0.69.2_Linux-64bit.deb
-
-sudo dpkg -i hugo_extended_0.69.2_Linux-64bit.deb
-
-rm -rf hugo_extended_0.69.2_Linux-64bit.deb
-
-# TMPDIR=$(mktemp -d)
-TMPDIR='/tmp'
 GH_PAGES_DIR=../gh-pages
 
-pipenv sync
-
-pipenv run python3 ./scripts/gen_repo_table.py
-
 cd build-dashboard
-  git submodule update --init --recursive themes/minimal
+  rm -rf ../gh-pages
+  mkdir -p ../gh-pages
 
-  rsync -avzh overlay/ themes
-  cp overlay/themes/minimal/static/css/main.css themes/themes/minimal/static/css/main.css
-
-  hugo --minify --enableGitInfo --ignoreCache -d $GH_PAGES_DIR
+  echo helloworld > ../gh-pages/index.html
 
 cd ..
-
-# git worktree list
-
-# git worktree remove $GH_PAGES_DIR
-
-# git worktree list
-
-# rm -rf $GH_PAGES_DIR
 
 #done
