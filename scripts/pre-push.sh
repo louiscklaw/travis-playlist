a4c2ca33b1bbc7f624c2750369592e1b16f3eb41
louiscklaw <louiscklaw@gmail.com>
2020-07-28 00:54:01 +0800

    ci: update to ignore dockerlint and travis lint when git push,

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -4,22 +4,22 @@ set -ex
 
 scripts/pre-commit.sh
 
-# scripts/test_dashboard.sh
+# # scripts/test_dashboard.sh
 
-for l in $(find . -name .travis.yml -not -path "./node_modules/*"  -not -path "./build-dashboard/node_modules/*" );
-do
-  travis lint $l &
-done;
+# for l in $(find . -name .travis.yml -not -path "./node_modules/*"  -not -path "./build-dashboard/node_modules/*" );
+# do
+#   travis lint $l &
+# done;
 
-for VARIABLE in `find . -name "dockerfile"`;
-do
-  dockerlint $VARIABLE &
-done
+# for VARIABLE in `find . -name "dockerfile"`;
+# do
+#   dockerlint $VARIABLE &
+# done
 
-wait
+# wait
 
-cd travis-build-merger
-  pipenv sync
-  # pipenv run python3 test.py
-  pipenv run python3 ./test/test.py
-cd -
+# cd travis-build-merger
+#   pipenv sync
+#   # pipenv run python3 test.py
+#   pipenv run python3 ./test/test.py
+# cd -
