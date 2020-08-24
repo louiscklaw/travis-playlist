da4c454e36ea7147e6e13548fd4bb7a6cc84d02f
louiscklaw <louiscklaw@gmail.com>
2020-08-24 19:44:46 +0800

    ci: update for build-dashboard,

--- build-dashboard/src/components/branch_fail_line_chart.js
+++ build-dashboard/src/components/branch_fail_line_chart.js
@@ -2,47 +2,106 @@ import React from 'react';
 import {Line} from 'react-chartjs-2';
 import {getBranchFailStatistics} from '../endpoints/jsons'
 
-const basic_config = {
+const chart_options={
+  tooltips: {
+    mode: 'index',
+    intersect: false
+  },
+  scales:{
+    yAxes:[{
+      ticks:{
+        beginAtZero: true
+      }
+    }]
+  }
+}
+
+const root_config = {
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
   datasets: [
     {
-      label: 'My First dataset',
+      label: 'number of repo',
       fill: false,
       lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
+      backgroundColor: 'rgba(192, 57, 43,0.4)',
+      borderColor: 'rgba(192, 57, 43,1)',
       borderCapStyle: 'butt',
       borderDash: [],
       borderDashOffset: 0.0,
       borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBorderColor: 'rgba(192, 57, 43,1)',
       pointBackgroundColor: '#fff',
       pointBorderWidth: 1,
       pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBackgroundColor: 'rgba(192, 57, 43,1)',
       pointHoverBorderColor: 'rgba(220,220,220,1)',
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
       data: [65, 59, 80, 81, 56, 55, 40]
+    },
+    {
+      label: 'number of branch',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(243, 156, 18,0.4)',
+      borderColor: 'rgba(243, 156, 18,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(243, 156, 18,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(243, 156, 18,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [1, 2, 3, 4, 5, 6, 7]
     }
   ]
 };
 
+
+
 function BranchFailLineChart(props){
   let [branch_fail_stat_json, setBranchFailStatJson] = React.useState(null)
+  let [chart_data, setChartData] = React.useState(root_config)
   React.useEffect(()=>{
     getBranchFailStatistics()
       .then(r => r.json())
       .then(r_json => {
         setBranchFailStatJson(r_json)
+        let record_times = r_json.map(x => x.t)
+        let number_of_repo = r_json.map(x => x.n)
+        let number_of_branch = r_json.map(x => x.f)
+
+        setChartData({
+          ...root_config,
+          labels: record_times,
+          datasets:[
+            {
+              ...root_config.datasets[0],
+              data: number_of_repo
+            },
+            {
+              ...root_config.datasets[1],
+              data: number_of_branch
+            }
+          ]
+        })
+
       })
+
+
   },[])
 
   return (
     <div>
       <h2>branch fail line chart</h2>
-      <Line data={basic_config} />
+      <Line data={chart_data} options={chart_options}/>
     </div>
   );
 
