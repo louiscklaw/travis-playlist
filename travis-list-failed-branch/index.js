2728468b09e41b9bbef54c651d7b87b13bfcdae7
louiscklaw <louiscklaw@gmail.com>
2020-08-24 21:05:05 +0800

    update,

--- travis-list-failed-branch/index.js
+++ travis-list-failed-branch/index.js
@@ -48,11 +48,8 @@ Promise.all( [
       last_builds_failed[repo_name]=failed_list
     })
 
-    return last_builds_failed
-  } )
-  .then( (last_builds_failed) => {
     console.log(last_builds_failed)
 
-    updateBuildFaliledList(last_builds_failed)
     addRecord(runStatistics(last_builds_failed))
-  })
+    updateBuildFaliledList(last_builds_failed)
+  } )
