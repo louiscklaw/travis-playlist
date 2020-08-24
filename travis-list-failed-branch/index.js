9a1dfda6405c5bd5946bc5e26b3cd58aeb04262d
louiscklaw <louiscklaw@gmail.com>
2020-08-24 20:57:38 +0800

    ci: update to remove test steps,

--- travis-list-failed-branch/index.js
+++ travis-list-failed-branch/index.js
@@ -48,11 +48,11 @@ Promise.all( [
       last_builds_failed[repo_name]=failed_list
     })
 
-    updateBuildFaliledList(last_builds_failed)
-
+    return last_builds_failed
   } )
-  .then( () => {
+  .then( (last_builds_failed) => {
+    console.log(last_builds_failed)
 
+    updateBuildFaliledList(last_builds_failed)
     addRecord(runStatistics(last_builds_failed))
-    // fs.writeFileSync('./answer.json',JSON.stringify(last_builds_failed),{encoding:'utf-8'})
   })
