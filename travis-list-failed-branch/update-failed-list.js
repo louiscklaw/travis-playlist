903f09c7c296c4675ef72b747d7432a3fdb5c06a
louiscklaw <louiscklaw@gmail.com>
2020-08-24 21:44:31 +0800

    update,

--- travis-list-failed-branch/update-failed-list.js
+++ travis-list-failed-branch/update-failed-list.js
@@ -50,12 +50,7 @@ Promise.all( [
 
   } )
   .then(() => {
+    console.log('updating build failed list ... ')
+    updateBuildFaliledList(last_builds_failed)
     // NOTE: need to update db by hubdb one by one (hubdb)
-    console.log('updating statistics ... ')
-    addRecord(runStatistics(last_builds_failed))
   })
-  // .then(() => {
-  //   console.log('updating build failed list ... ')
-  //   updateBuildFaliledList(last_builds_failed)
-  //   // NOTE: need to update db by hubdb one by one (hubdb)
-  // })
