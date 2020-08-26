721d2a1b0ab1d3cef00d7a64d67eca9d4fdf56f3
louiscklaw <louiscklaw@gmail.com>
2020-08-26 14:00:49 +0800

    ci: refactor pre-push.sh,

--- travis-check-leak/main.py
+++ travis-check-leak/main.py
@@ -7,8 +7,9 @@ import logging
 import traceback
 from pprint import pprint
 import shlex
-
 from subprocess import run, PIPE, Popen
+
+from tqdm import tqdm, trange
 
 def checkPythonVersion():
   import platform
@@ -37,7 +38,9 @@ def checkLeak(should_not_appear, filepath_to_check):
   else:
     # e.g. 3.6 for ubuntu 18.04
     result = run(command, stdout=PIPE)
-    print(result.stdout)
+
+    # DEBUG:
+    # print(result.stdout)
 
   terms_found = result.stdout != b''
 
@@ -105,15 +108,16 @@ def printBanner(text, text1):
 
 
 def main():
-    printBanner('scanning for sensitive words', SCAN_DIR)
     should_not_appear = list(credentialValue())
 
-    print('num of sensitive word {}'.format(len(should_not_appear)))
+    printBanner('scanning for sensitive words', SCAN_DIR)
     print('scan start')
-    for word in should_not_appear:
+
+    for word in tqdm(should_not_appear):
       if word not in SKIP_LIST:
         try:
           checkLeak(word, SCAN_DIR)
+
         except Exception as e:
           print('sensitive word found')
           raise e
