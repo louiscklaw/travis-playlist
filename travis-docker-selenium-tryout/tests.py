0db12220dbac5e8b8ca682272d282551774f5e71
louiscklaw <louiscklaw@gmail.com>
2020-08-27 23:45:11 +0800

    update html test page,

--- travis-docker-selenium-tryout/tests.py
+++ travis-docker-selenium-tryout/tests.py
@@ -26,7 +26,7 @@ class GrammarlyGeneralTest(unittest.TestCase):
     def test_login(self):
         page_login = GrammarlyLogin(self.driver)
         page_login.make_login('za2217279@mvrht.net', 'test123')
-        assert self.driver.current_url == "https://louiscklaw.github.io/html-example-page/signin.html"
+        assert self.driver.current_url == "https://louiscklaw.github.io/html-example-page/signin_ok.html"
 
     # def test_make_new_doc(self):
     #     page_login = GrammarlyLogin(self.driver)
