95671c33b922406b64e95dee24a807f5759c7fff
louiscklaw <louiscklaw@gmail.com>
2020-08-28 19:29:48 +0800

    ci: init change and test,

--- travis-appium-android-tryout/test.sh
+++ travis-appium-android-tryout/test.sh
@@ -3,10 +3,10 @@
 set -ex
 
 cd travis-appium-android-tryout
-  docker-compose pull 2>&1
-  # docker-compose build
-  # docker-compose up -d
-  # sleep 10
-  # docker-compose logs
+  docker-compose pull
+  docker-compose build
+  docker-compose up -d
+  sleep 10
+  docker-compose logs
 
 cd ..
