20a1463dc06282d5922d3d83e7cda197f2c259b9
louiscklaw <louiscklaw@gmail.com>
2020-08-28 19:28:17 +0800

    ci: poc test scripts,

---
+++ travis-appium-android-tryout/helloworld_hub.js
@@ -0,0 +1,35 @@
+const webdriver = require('selenium-webdriver');
+
+const GRID_HOST = 'localhost:4444/wd/hub';
+
+function searchTextOnGoogle() {
+  // Setup Input capabilities
+  const capabilities = {
+    platformName: "Android",
+    platformVersion: "7.1.1",
+    browserName: 'android',
+    // app: "/root/apk_pool/ApiDemos-debug.apk",
+  }
+
+  // URL: https://{username}:{accessKey}@hub.lambdatest.com/wd/hub
+  const gridUrl = 'http://' + GRID_HOST;
+
+  // setup and build selenium driver object
+  const driver = new webdriver.Builder()
+      .usingServer(gridUrl)
+      .withCapabilities(capabilities)
+      .build();
+
+  // navigate to a url, search for a text and get title of page
+  driver.get('https://www.google.com/ncr').then(function() {
+      driver.findElement(webdriver.By.name('q')).sendKeys('LambdaTest\n').then(function() {
+          driver.getTitle().then(function(title) {
+              setTimeout(function() {
+                  console.log(title);
+                  driver.quit();
+              }, 5000);
+          });
+      });
+  });
+}
+searchTextOnGoogle();
