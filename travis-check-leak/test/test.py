10d0d36ccb1af0019110dd1543644eadb1991cf3
louiscklaw <louiscklaw@gmail.com>
2020-09-09 16:45:34 +0800

    ci: update check leak,

--- travis-check-leak/test/test.py
+++ travis-check-leak/test/test.py
@@ -2,6 +2,8 @@
 
 import os, sys
 from pprint import pprint
+from tempfile import mkdtemp
+from subprocess import run, PIPE, Popen
 
 import unittest
 
@@ -23,8 +25,14 @@ class TestStringMethods(unittest.TestCase):
     self.assertEqual((True,'aaa'),result,'error with leak found')
 
   def test_callingWithoutPath(self):
-    pass
+    result = run(shlex.split('python3 main.py'))
+    self.assertEqual(result.returncode,99, 'should return error 99 when calling without input path')
 
+  def test_callingWithPath_NoWordFound(self):
+    command = 'python3 main.py {}'.format(mkdtemp())
+    pprint(command)
+    result = run(shlex.split(command))
+    self.assertEqual(result.returncode,0, 'should return 0 when no sensitive word found')
 
 if __name__ == '__main__':
   unittest.main()
