2afdbcbe4eec6bf59a3ea32324175127a7c88043
louiscklaw <louiscklaw@gmail.com>
2020-09-14 19:09:28 +0800

    ci: update main build chain,

--- travis-failed-rebuild-tryout/src/lib/triggerBuildOnTravis.js
+++ travis-failed-rebuild-tryout/src/lib/triggerBuildOnTravis.js
@@ -9,11 +9,11 @@ function triggerBuildOnTravis(repo_name, branch_name){
   const travis_url = travisRequestUrl(repo_slug)
   const req_body = getRequestBody(branch_name)
 
+
   return fetchTravisRequestBuild(travis_url, req_body)
     .then( r => r.json())
 
 }
-
 
 module.exports={
   triggerBuildOnTravis
